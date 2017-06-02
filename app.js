59a5e0581b8190f98bcaec73a61aec8d382baba5
threebrick <darnell@threebrick.com>
2017-06-02 01:14:16 -0400

    test 3

--- app.js
+++ app.js
@@ -21,64 +21,13 @@ var connector = new builder.ChatConnector({
 server.post('https://rpabot.azurewebsites.net/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector);
-
-
-bot.dialog('/', [
-    function (session) {
-        // Add an InputHint to let Cortana know to expect user input
-      //  session.say('Hi there', '<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US">Hi, what’s your name?</speak>', {
-      //      inputHint: builder.InputHint.expectingInput
-      //  });
-        builder.Prompts.text(session, 'Hi there.  What is your name?', {            
-        speak: 'Hey there mister.  What is your name ?',
-        retrySpeak: 'I did not understand my man.  Say your name?',
-        inputHint: builder.InputHint.expectingInput
-        });
-    },
-    function (session, results) {
-        //session.say("You said your name is '%s'", results.response.entity);
-        //console.log(session.message.text);
-
-        builder.Prompts.text(session, 'You said '+ session.message.text, {            
-        speak: 'You said ' + session.message.text +''
-        
-        });
-
- //       var msg = new builder.Message(session)
-//            .attachments([
- //               new builder.HeroCard(session)
- //                   .title("Your name")
-//                    .subtitle("You said that your name was "+ session.message.text)
-//                    .images([
-//                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-//                    ])
-////                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
- //           ]);
-        //session.send(msg);
-//        session.endDialog(msg);
 
 
 
-
-//var card = new builder.HeroCard(session)
-//        .title('Testing Cortana Skills')
-//        .buttons([
-//            builder.CardAction.imBack(session, 'events', 'Find Events'),
-//            builder.CardAction.imBack(session, 'businesses', 'Search Businesses')
-//        ]);
-//    var msg = new builder.Message(session)
-//        .speak(speak(session, 'You said that your name was'))
-//        .addAttachment(card)
-//        .inputHint(builder.InputHint.acceptingInput); // Tell Cortana to accept input
-//    session.send(msg).endDialog();
-
-        
-    }
-]);
-
-
-
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
 
 
 
